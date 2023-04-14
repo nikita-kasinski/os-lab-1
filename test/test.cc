353773a3d2185867ddb465d549e16e5802796e4c
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-14 20:42:13 +0300

    add unit tests for safePositiveDoubleInput

--- test/test.cc
+++ test/test.cc
@@ -21,3 +21,25 @@ TEST(TestSafeUnsignedIntegerInput, TestNegativeInput)
     a = Controller::safeUnsignedIntegerInput(testStream, ostream, "", "");
     EXPECT_EQ(a, 2);
 }
+
+TEST(TestSafePositiveDoubleInput, TestFailedInput)
+{
+    std::istringstream testStream("asdfbsda1\n2.3 sadgsadg123gasd\n 3.7\n");
+    std::ostringstream ostream;
+    constexpr double delta = 1e-9;
+    double a, b;
+    a = Controller::safePositiveDoubleInput(testStream, ostream, "", "");
+    b = Controller::safePositiveDoubleInput(testStream, ostream, "", "");
+    EXPECT_TRUE(std::abs(a - 2.3) < delta);
+    EXPECT_TRUE(std::abs(b - 3.7) < delta);
+}
+
+TEST(TestSafePositiveDoubleInput, TestNegativeInput)
+{
+    std::istringstream testStream("skldghuhasdkjgn\n -1 \n2.7\n");
+    std::ostringstream ostream;
+    constexpr double delta = 1e-9;
+    double a;
+    a = Controller::safePositiveDoubleInput(testStream, ostream, "", "");
+    EXPECT_TRUE(std::abs(a - 2.7) < delta);
+}
