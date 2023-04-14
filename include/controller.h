2b8fcea362658d5a6f5322ff264d59f4d49ec316
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-14 19:55:10 +0300

    declare safeDoubleInput method

--- include/controller.h
+++ include/controller.h
@@ -16,6 +16,12 @@ public:
         const std::string &inputPrompt,
         const std::string &inputFailedPrompt);
 
+    static size_t safeDoubleInput(
+        std::istream &in,
+        std::ostream &out,
+        const std::string &inputPrompt,
+        const std::string &inputFailedPrompt);
+
     static bool printBinaryFile(const std::string &binFile, std::ostream &out);
 
     static bool printReportFile(const std::string &reportFile, std::ostream &out);
