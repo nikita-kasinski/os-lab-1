4fa8ea3d96e5c7104f7a36a2b3cb364f872713f1
Nikita Kasinski <kasinski.nikita@gmail.com>
2023-04-14 21:04:15 +0300

    add unit tests for binary and report file processing

--- test/test.cc
+++ test/test.cc
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include <sstream>
+#include <cstring>
+#include <string>
+#include "employee.h"
 #include "controller.h"
 
 TEST(TestSafeUnsignedIntegerInput, TestFailedInput)
@@ -42,4 +45,102 @@ TEST(TestSafePositiveDoubleInput, TestNegativeInput)
     double a;
     a = Controller::safePositiveDoubleInput(testStream, ostream, "", "");
     EXPECT_TRUE(std::abs(a - 2.7) < delta);
+}
+
+TEST(TestPrintCreateBinaryFile, TestStandartFlow)
+{
+    size_t size = 3;
+    std::string binFile = "binFile";
+    Employee *employees = new Employee[size];
+    std::ostringstream out;
+    std::ostringstream expectedAnswer;
+    std::string names[3] = {"vasya", "vitya", "kostya"};
+    size_t hours[3] = {3, 5, 7};
+    size_t nums[3] = {0, 1, 2};
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        expectedAnswer
+            << std::setw(Controller::numWidth) << nums[i]
+            << std::setw(Controller::nameWidth) << names[i]
+            << std::setw(Controller::hoursWidth) << hours[i] << "\n";
+    }
+
+    employees[0].num = 0;
+    strcpy(employees[0].name, "vasya");
+    employees[0].hours = 3;
+
+    employees[1].num = 1;
+    strcpy(employees[1].name, "vitya");
+    employees[1].hours = 5;
+
+    employees[2].num = 2;
+    strcpy(employees[2].name, "kostya");
+    employees[2].hours = 7;
+
+    EXPECT_TRUE(Controller::createBinaryFile(binFile, employees, size));
+    EXPECT_TRUE(Controller::printBinaryFile(binFile, out));
+
+    EXPECT_EQ(out.str(), expectedAnswer.str());
+    delete[] employees;
+}
+
+TEST(TestPrintBinaryFile, TestNoFile)
+{
+    std::ostringstream out;
+    EXPECT_FALSE(Controller::printBinaryFile("abacaba", out));
+}
+
+TEST(TestPrintCreateReportFile, TestStandartFlow)
+{
+    size_t size = 3;
+    double payment = 5.6;
+    std::string binFile = "binFile";
+    std::string reportFile = "reportFile.txt";
+    Employee *employees = new Employee[size];
+    std::ostringstream out;
+    std::ostringstream expectedAnswer;
+    std::string names[3] = {"vasya", "vitya", "kostya"};
+    size_t hours[3] = {3, 5, 7};
+    size_t nums[3] = {0, 1, 2};
+
+    expectedAnswer
+        << std::setw(Controller::numWidth) << "id"
+        << std::setw(Controller::nameWidth) << "name"
+        << std::setw(Controller::hoursWidth) << "hours"
+        << std::setw(Controller::paymentWidth) << "payment"
+        << "\n";
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        expectedAnswer
+            << std::setw(Controller::numWidth) << nums[i]
+            << std::setw(Controller::nameWidth) << names[i]
+            << std::setw(Controller::hoursWidth) << hours[i]
+            << std::setw(Controller::paymentWidth) << hours[i] * payment << "\n";
+    }
+
+    employees[0].num = 0;
+    strcpy(employees[0].name, "vasya");
+    employees[0].hours = 3;
+
+    employees[1].num = 1;
+    strcpy(employees[1].name, "vitya");
+    employees[1].hours = 5;
+
+    employees[2].num = 2;
+    strcpy(employees[2].name, "kostya");
+    employees[2].hours = 7;
+
+    EXPECT_TRUE(Controller::createBinaryFile(binFile, employees, size));
+    EXPECT_TRUE(Controller::createReportFile(binFile, reportFile, payment));
+    EXPECT_TRUE(Controller::printReportFile(reportFile, out));
+
+    EXPECT_EQ(out.str(), expectedAnswer.str());
+    delete[] employees;
+}
+
+TEST(TestPrintReportFile, TestNoFile)
+{
+    EXPECT_FALSE(Controller::createReportFile("abacaba", "report.txt", 5));
 }
